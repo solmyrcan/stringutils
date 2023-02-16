10836655be3b05ffa95538ee699f5db580df01af
solmyrcan <90778405+solmyrcan@users.noreply.github.com>
2023-02-16 18:50:21 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package stringutils
+
+import (  
+  "fmt"
+  "strings"
+)
+
+func Upper(text string) string {
+  return strings.ToUpper(text)
+}
+
+func Lower(text string) string {
+  return strings.ToLower(text)
+}
